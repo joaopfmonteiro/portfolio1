ab16d229746421d0cda6f2ee4d103c2354f1dd60
Joao Pereira Ferreira Monteiro <96503806+joaopfmonteiro@users.noreply.github.com>
2022-01-22 01:50:23 +0000

    Update about.js

--- src/about.js
+++ src/about.js
@@ -6,9 +6,12 @@ export default()=>{
         <div className="about">
         <div className="about-title">About Me</div>
         <div className="about-discription">Hi! I'm <span className="my-name"> João Monteiro </span>
-         a Front-End developer orifinally from Nazaré, Portugal. IT lover with a serious passion for ReactJS.
-         Well-organised person, problem solver, creative, high attention to detail to provide
-         you can find me somewere in the ocean trying to cath the perfect wave.
+         a Front-End developer originally from Nazaré, Portugal. IT lover with a serious passion for ReactJS.
+         Well-organised person, problem solver, creative, high attention to detail to provide hight-impact web
+         solutions incorporate a rage of technologies.
+         I have expirience with JavaScript, ReactJS, CSS, HTML, Jira and GitHub.
+         When I'm not coding, you can find me somewere in the ocean trying to catch the perfect wave or  
+
           </div>
         </div>
     )
