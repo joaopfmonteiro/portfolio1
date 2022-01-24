a2c50da291ad6a8f76afd80565fce5cea1a56121
Joao Pereira Ferreira Monteiro <96503806+joaopfmonteiro@users.noreply.github.com>
2022-01-24 16:18:59 +0000

    PORT-6
    Updated PopUp

--- src/Work.js
+++ src/Work.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
+import Popup from './popup';
 
 export default()=>{
+    const[buttonPopup, setButtonPopup] = useState(false);
     return(
         <div className='react'>
             <div className='react-title'>React</div>
             <div className='react-work'>
-                <div className='netflix'></div>
-                <div className='hoqueiflix'></div>
-                <div className='portfolio'></div>
+                <div className='netflix'>netflix</div>
+                <div className='hoqueiflix'>hoqueiflix</div>
+                <div className='portfolio'>portfolio</div>
             </div>
+            <button onClick={() => setButtonPopup(true)}>open popup</button>
+            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+            <h3>My popup</h3>
+            </Popup>
         </div>
     )
 }
