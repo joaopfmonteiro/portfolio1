5915c5be5378925ef6a461f1d3d06d5b011552d2
Joao Pereira Ferreira Monteiro <96503806+joaopfmonteiro@users.noreply.github.com>
2022-01-26 19:48:06 +0000

    updated

--- src/Work.js
+++ src/Work.js
@@ -9,20 +9,22 @@ export default()=>{
         <div className='react'>
             <div className='react-work'>
                 <div className='netflix'>
-                    <div onClick={() => setButtonPopup(true)}><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/1.png?alt=media&token=07191321-d285-4071-8f32-a4ff883c64f7"/></div>
+                    <div className='netflixFront'onClick={() => setButtonPopup(true)}><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/1.png?alt=media&token=07191321-d285-4071-8f32-a4ff883c64f7"/></div>
                     <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
                     <h2 className='netflixBack'>
                     <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/Design%20sem%20nome%20(3).png?alt=media&token=acebd199-b150-4e35-9c94-413b367ae974"/></h2>
                     </Popup></div>
                 <div className='hoqueiflix'>
-                    <div onClick={() => setButtonPopup(true)}><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/2.png?alt=media&token=c0d91130-58e2-4ef6-9c46-035832c0f37f"/></div>
+                    <div className='hoqueiflixFront'onClick={() => setButtonPopup(true)}><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/2.png?alt=media&token=c0d91130-58e2-4ef6-9c46-035832c0f37f"/></div>
                     <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-                    <h2 className='hoqueiflixBack'><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/Design%20sem%20nome.png?alt=media&token=f65e9ae9-1da6-44fa-a0a7-2d316256b322"/></h2>
+                    <h2 className='hoqueiflixBack'>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/Design%20sem%20nome.png?alt=media&token=f65e9ae9-1da6-44fa-a0a7-2d316256b322"/></h2>
                     </Popup></div>
                 <div className='portfolio'>
-                    <div onClick={() => setButtonPopup(true)}><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/Log%C3%B3tipo%20Preto%20e%20Dourado%20C%C3%ADrculo%20Imobili%C3%A1ria.png?alt=media&token=51c8473b-9dfc-4b1f-b02d-c5bcc26d1400"/></div>
+                    <div className='portfolioFront'onClick={() => setButtonPopup(true)}><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/Log%C3%B3tipo%20Preto%20e%20Dourado%20C%C3%ADrculo%20Imobili%C3%A1ria.png?alt=media&token=51c8473b-9dfc-4b1f-b02d-c5bcc26d1400"/></div>
                     <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-                    <h4 className='portfolioBack'><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/Design%20sem%20nome.png?alt=media&token=f65e9ae9-1da6-44fa-a0a7-2d316256b322"/></h4>
+                    <h2 className='portfolioBack'>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-cff15.appspot.com/o/Design%20sem%20nome.png?alt=media&token=f65e9ae9-1da6-44fa-a0a7-2d316256b322"/></h2>
                     </Popup></div>
             </div>
         </div>
