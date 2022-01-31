b3b791bd80d399eee493d495ce8a74807b5ba5b3
Joao Pereira Ferreira Monteiro <96503806+joaopfmonteiro@users.noreply.github.com>
2022-01-31 16:02:23 +0000

    update

--- src/body.js
+++ src/body.js
@@ -1,7 +1,7 @@
 import React from "react";
 import  "./body.css";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+
 
 export default () => {
     return(
@@ -15,22 +15,6 @@ export default () => {
     <a href="./contact">
     <b className="name2"></b>
     </a>
-
-
-
-
-    
-    {/* <b className="introduction">
-        Front-End Developer
-    </b>
-    <b className="job--logo">
-        <b className="log--gitHub">
-            <a href="https://github.com/joaopfmonteiro" target="_blank" ><GitHubIcon/></a>
-        </b>
-        <b className="log--linkdin">
-            <a href="https://www.linkedin.com/in/joao-pereira-monteiro3/" target="_blank"><LinkedInIcon/></a>
-        </b>
-    </b> */}
 </div>
     )
 
